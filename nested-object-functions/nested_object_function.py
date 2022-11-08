31f930f419968ddc4da5ca9256edbbfbb3d11821
venkatpulagam <117551826+venkatpulagam@users.noreply.github.com>
2022-11-08 09:03:08 +0530

    Update nested_object_function.py

--- nested-object-functions/nested_object_function.py
+++ nested-object-functions/nested_object_function.py
@@ -1,3 +1,12 @@
+##
+# Example Inputs
+# object = {“a”:{“b”:{“c”:”d”}}}
+# key = a/b/c
+##
+# object = {“x”:{“y”:{“z”:”a”}}}
+# key = x/y/z
+# value = a
+
 def getKey(obj: dict):
     keys = list(obj)
     if len(keys) != 1:
